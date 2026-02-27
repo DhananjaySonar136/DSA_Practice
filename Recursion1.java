ce4facd07f7456cb4b85d5253ec169dbfca21049
DhananjaySonar136 <sonar8187@gmail.com>
2026-02-27 23:25:52 +0530

    recursion topic started

--- Recursion1.java
+++ Recursion1.java
@@ -5,7 +5,14 @@ public class Recursion1 {
         fun();
     }
 
-    
+    int count=1;
+    void print(){
+        System.out.print(count);
+        count++;
+        if(count==5) return ;
+        print();
+        System.out.println(count--);
+    }
     public static void main(String[] args) {
         Recursion1 r1=new Recursion1();
         r1.print();
