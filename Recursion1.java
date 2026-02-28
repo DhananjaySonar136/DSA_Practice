b534c0dc38a330386b58eefab002b2cb2501dfe0
DhananjaySonar136 <sonar8187@gmail.com>
2026-02-28 17:59:09 +0530

    code added0

--- Recursion1.java
+++ Recursion1.java
@@ -9,12 +9,42 @@ public class Recursion1 {
     void print(){
         System.out.print(count);
         count++;
-        if(count==5) return ;
+        if(count==5) return;
         print();
         System.out.println(count--);
     }
-    public static void main(String[] args) {
+
+    void namePrint(int n,int count){
+        if(count>=n){
+            return;
+        }
+        System.out.println("Dhananjay");
+        namePrint(n,count+1);
+    }
+
+    void print1toN(int n,int i){
+        if(i>n) return;
+        System.out.println((i));
+        print1toN(n, i+1);
+    }
+
+    void printNto1(int n){
+        if(1>n) return;
+        System.out.println(n);
+        printNto1(n-1);
+    }
+
+    //BackTracking
+    //print from 1 to N by using backtracking
+    void f1toN(int n){
+        if(n<1) return ;
+        f1toN(n-1);
+        System.out.println(n);
+    }
+
+
+        public static void main(String[] args) {
         Recursion1 r1=new Recursion1();
-        r1.print();
+        r1.fNto1(10,1);
     }
 }
