2cb5b57f2cd00714baa22a394522c21da038ffb9
DhananjaySonar136 <sonar8187@gmail.com>
2026-03-06 11:26:30 +0530

    code added

--- Arrays2.java
+++ Arrays2.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.TreeSet;
 
 public class Arrays2 {
 
@@ -56,11 +57,68 @@ public class Arrays2 {
         }
     }
 
+    void moveZerosToEnd(int arr[]){
+        int n=arr.length;
+        int temp[]=new int[n];
+        int t=0;
+
+        for(int i=0;i<n;i++){
+            if(arr[i]!=0){
+                temp[t]=arr[i];
+                t++;
+            }
+        }
+
+        for(int i=0;i<temp.length;i++){
+            arr[i]=temp[i];
+        }
+
+        int zeros=arr.length-temp.length;
+        for(int i=temp.length;i<n;i++){
+            arr[i]=0;
+        }
+
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    int linearSearch(int arr[],int key){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //union of two sorted array
+    void unionarray(int arr1[],int arr2[]){
+        
+        TreeSet<Integer> ts=new TreeSet<>();
+        
+        for(int i=0;i<arr1.length;i++){
+            ts.add(arr1[i]);
+        }
+
+        for(int i=0;i<arr2.length;i++){
+            ts.add(arr2[i]);
+        }
+
+        Object union[]=ts.toArray();
+        for(int i=0;i<union.length;i++){
+            System.out.print(union[i]);
+        }
+    }
     public static void main(String[] args) {
         
         Arrays2 a2=new Arrays2();
-        int arr[]={1,2,3,4,5,6,7};
-        a2.leftRotate1(arr, 3);
+        int arr1[]={1,0,2,0,0,3,4,0,5,6,0,7};
+        int arr2[]={10,3,2,8};
+        a2.unionarray(arr1, arr2);
+        // System.out.println(a2.linearSearch(arr, 7));
+        // a2.moveZerosToEnd(arr);
+        // a2.leftRotate1(arr, 3);
         // a2.rotateleft(arr);
     }
 }
