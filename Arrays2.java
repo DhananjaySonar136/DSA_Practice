37cb39f74db8657a7ed42bbce919384ab125cfea
DhananjaySonar136 <sonar8187@gmail.com>
2026-03-08 23:09:34 +0530

    DSA Array

--- Arrays2.java
+++ Arrays2.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.TreeSet;
 
@@ -92,30 +93,125 @@ public class Arrays2 {
         return -1;
     }
 
+    
     //union of two sorted array
-    void unionarray(int arr1[],int arr2[]){
+    // Both the array must be sorted
+    ArrayList<Integer> unionarray(int arr1[],int arr2[]){
         
-        TreeSet<Integer> ts=new TreeSet<>();
+        // TreeSet<Integer> ts=new TreeSet<>();
         
-        for(int i=0;i<arr1.length;i++){
-            ts.add(arr1[i]);
+        // for(int i=0;i<arr1.length;i++){
+        //     ts.add(arr1[i]);
+        // }
+
+        // for(int i=0;i<arr2.length;i++){
+        //     ts.add(arr2[i]);
+        // }
+
+        // Object union[]=ts.toArray();
+        // for(int i=0;i<union.length;i++){
+        //     System.out.print(union[i]);
+        // }
+
+        //ORRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR
+
+        int n1=arr1.length;
+        int n2=arr2.length;
+        ArrayList<Integer> list=new ArrayList<>();
+        int i=0;
+        int j=0;
+        while(i<n1 && j<n2){
+
+            //if arr1 element is small the add in list if previous element is not equal to current element
+            // i++ is important
+            if(arr1[i]<=arr2[j]){
+                if(list.size()==0 || list.get(list.size()-1)!=arr1[i])
+                    list.add(arr1[i]);
+                    i++;
+    
+            }
+
+            //if arr2 element is small the add in list if previous element is not equal to current element
+            // j++ is important
+            else if(arr2[j]<arr1[i]){
+                if(list.size()==0 || list.get(list.size()-1)!=arr2[j])
+                    list.add(arr2[j]);
+                    j++;
+                 
+            }
+
         }
 
-        for(int i=0;i<arr2.length;i++){
-            ts.add(arr2[i]);
+        //if index of j end then only work on i
+        while(i<n1){
+                if(list.size()==0 || list.get(list.size()-1)!=arr1[i])
+                    list.add(arr1[i]);
+                    i++;
+                
         }
 
-        Object union[]=ts.toArray();
-        for(int i=0;i<union.length;i++){
-            System.out.print(union[i]);
+        //if index of i end then only work on j
+        while(j<n2){
+                if(list.size()==0 || list.get(list.size()-1)!=arr2[j])
+                    list.add(arr2[j]);
+                    j++;
+                
         }
+
+        return list;
     }
+
+
+    ArrayList<Integer> findSortedArrayIntersection(int arr1[],int arr2[]){
+        // // take array to track wheather element is already there or not
+        // int visited[]=new int[arr1.length];
+        // ArrayList<Integer> list=new ArrayList<>();
+        // for(int i=0;i<arr1.length;i++){
+        //     for(int j=0;j<arr2.length;j++){
+        //         if(arr1[i]==arr2[j]  &&  visited[j]!=1){
+        //             list.add(arr1[i]);
+        //             visited[j]=1;
+        //             break;
+        //         }
+
+        //         if(arr2[j]>arr1[i]) break;
+        //     }
+        // }
+        // return list;
+
+        int i=0;
+        int j=0;
+        int n1=arr1.length;
+        int n2=arr2.length;
+        ArrayList<Integer> list=new ArrayList<>();
+
+        while(i<n1 && j<n2){
+            if(arr1[i]<arr2[j]){
+                i++;
+            }
+            else if(arr2[j]<arr1[i]){
+                j++;
+            }
+            else{
+                list.add(arr1[i]);
+                i++;
+                j++;
+            }
+        }
+        return list;
+    } 
+
+
+
+
     public static void main(String[] args) {
         
         Arrays2 a2=new Arrays2();
-        int arr1[]={1,0,2,0,0,3,4,0,5,6,0,7};
-        int arr2[]={10,3,2,8};
-        a2.unionarray(arr1, arr2);
+        int arr1[]={1,2,3,4,5,6,7};
+        int arr2[]={1,3,3,5,8};
+
+        System.out.println(a2.findSortedArrayIntersection(arr1, arr2));
+    //    System.out.println(a2.unionarray(arr1, arr2));
         // System.out.println(a2.linearSearch(arr, 7));
         // a2.moveZerosToEnd(arr);
         // a2.leftRotate1(arr, 3);
