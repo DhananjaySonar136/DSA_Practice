28a0ee6b8f694de34201f828f6b2b0b9958d19fc
DhananjaySonar136 <sonar8187@gmail.com>
2026-03-30 01:17:47 +0530

    Leetcode problems

---
+++ LeetCode/L977.java
@@ -0,0 +1,27 @@
+class L977 {
+    public int[] sortedSquares(int[] arr) {
+        
+        int i=0;
+        int j=arr.length-1;
+        int n=arr.length;
+        int pos=n-1;
+        int newarr[]=new int[n];
+
+        while(i<=j){
+
+            if(Math.abs(arr[i]) < Math.abs(arr[j])){
+                newarr[pos]=arr[j]*arr[j];
+                j--;
+            }
+            else{
+                newarr[pos]=arr[i]*arr[i];
+                i++;
+            }
+            pos--;
+        }
+
+        return newarr;
+        
+
+    }
+}
