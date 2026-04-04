808db8d50945f43cbc4248bb6e4c0c77393cbc7b
DhananjaySonar136 <sonar8187@gmail.com>
2026-04-05 00:17:30 +0530

    Add L11 Problem

---
+++ LeetCode/L11.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxArea(int[] height) {
+        
+        int left=0;
+        int right=height.length-1;
+        int maxWater=0;
+
+        while(left<=right){
+
+            //calculate the width between both the lines
+            int width=right-left;
+
+            //now find minimum from both line and multiply with width so that how much capacity water will store we can find
+            int watercap= width * Math.min(height[left],height[right]);
+            
+            //now find max water
+            maxWater=Math.max(maxWater,watercap);
+
+            if(height[left] < height[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+
+        return maxWater;
+    }
+}
