7d7cf731c4bbb4b8c87209d14db6c288030100b4
DhananjaySonar136 <sonar8187@gmail.com>
2026-05-07 13:23:18 +0530

    Buuble Sort;

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,22 @@
+package Sorting;
+
+import java.util.Arrays;
+
+class BubbleSort{
+
+    static void Bubble(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void main(String[] args) {
+        Bubble(new int[]{10,4,30,29,21});
+    }
+}
