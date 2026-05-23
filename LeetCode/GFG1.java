99c81720977bdd718b00108a189c11478fd934e5
DhananjaySonar136 <sonar8187@gmail.com>
2026-05-23 23:55:27 +0530

    .

--- LeetCode/GFG1.java
+++ LeetCode/GFG1.java
@@ -9,8 +9,7 @@ class Solution {
         
         long count=0L;
         for(int i=0;i<n-2;i++){
-            int j=i+1;
-            int k=n-1;
+           
             
             while(j<k){
                 
